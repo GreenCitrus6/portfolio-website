bd3c693dde1350db0f6a30b729e028813b99b085
Daniel A <DanielAAad01@gmail.com>
2023-12-11 02:08:33 -0800

    Added styling to header, added more detailed comments planning out about and contact sections.

--- src/app/header.js
+++ src/app/header.js
@@ -1,13 +1,13 @@
 
 export default function Header() {
     return (
-        <header className="bg-stone-300 text-stone-700">
-            <navbar className="flex flex-row">
+        <header className="bg-stone-300 text-stone-700 h-16 flex overflow-hidden">
+            <nav className="flex flex-row items-center ml-4 sm:ml-36">
                 {/* nav will have About, Portfolio, Contact. will be fixed to top of page */}
-                <a>About</a>
-                <a>Projects</a>
-                <a>Contact</a>
-            </navbar>
+                <a className="w-24 text-center" href="#aboutSection">About</a>
+                <a className="w-24 text-center" href="#portfolioSection">Projects</a>
+                <a className="w-24 text-center" href="#contactSection">Contact</a>
+            </nav>
         </header>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,21 @@ export default function Home() {
     <>
       <Header />
       <main className="flex flex-col text-gray-800">
-        <section id="aboutSection">
+        <section id="aboutSection" className="">
           {/* about section */}
-          <h2>Hello</h2>
-          <h1>I'm Daniel</h1>
-          <h3>and I do Web Development</h3>
+          <h1>Hello, I'm Daniel</h1>
+          <h2>and I do Web Development</h2>
+          <div id="explanationContainer">
+            <p >This website was made with Next.js</p>
+            <a className="text-gray-400 italic">Click <span className="text-amber-500 hover:text-amber-400 active:text-amber-300">here</span> to learn about all the techonologies used to make this page</a>
+            {/* make a modal window that pops up and shows the technologies used in this page:
+            react.js with next.js
+            tailwind.css*/}
+          </div>
         </section>
-        <section id="portfolioSection">
-          {/* portfolio, cards that have an option to view an explanation of what they are and what tech they use, or link to the deployed project to try it 
+        <section id="portfolioSection" className="">
+          <h2>Projects</h2>
+          {/* portfolio, cards that have an option to view a modal window with an explanation of what they are and what tech they use, or link to the deployed project to try it 
             journal app with backend
             pokemon sleep collection manager
             react todo list,
@@ -24,7 +31,8 @@ export default function Home() {
           */}
           <ProjectCard /> 
         </section>
-        <section id="contactSection">
+        <section id="contactSection" className="">
+          <h2>Check out more of my work</h2>
           {/* contact info, including linkedin and github */}
 
         </section>
